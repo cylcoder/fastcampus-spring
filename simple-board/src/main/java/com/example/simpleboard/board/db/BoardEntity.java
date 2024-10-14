ea6091fb282250d7248455a9d30e8edd1d3ddba1
cylcoder <cylcoder@gmail.com>
2024-10-14 23:55:01 +0900

    Chapter 8-3 Entity 개발

---
+++ simple-board/src/main/java/com/example/simpleboard/board/db/BoardEntity.java
@@ -0,0 +1,24 @@
+package com.example.simpleboard.board.db;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+@Entity(name = "board")
+public class BoardEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String boardName;
+
+    private String status;
+
+}
---
+++ simple-board/src/main/java/com/example/simpleboard/post/db/PostEntity.java
@@ -0,0 +1,39 @@
+package com.example.simpleboard.post.db;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+@Entity(name = "post")
+public class PostEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long boardId;
+
+    private String userName;
+
+    private String password;
+
+    private String email;
+
+    private String status;
+
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime postedAt;
+
+}
