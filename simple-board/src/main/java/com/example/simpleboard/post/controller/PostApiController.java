39cc5593d976c5e9244baddcd217578f1c1ba9fb
cylcoder <cylcoder@gmail.com>
2024-10-15 00:45:36 +0900

    Chapter 8-4 API Endpoint 개발1

---
+++ simple-board/src/main/java/com/example/simpleboard/post/controller/PostApiController.java
@@ -0,0 +1,25 @@
+package com.example.simpleboard.post.controller;
+
+import com.example.simpleboard.post.db.PostEntity;
+import com.example.simpleboard.post.model.PostRequest;
+import com.example.simpleboard.post.service.PostService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/post")
+public class PostApiController {
+
+    private final PostService postService;
+
+    @PostMapping
+    public PostEntity create(@Valid @RequestBody PostRequest postRequest) {
+        return postService.create(postRequest);
+    }
+
+}
