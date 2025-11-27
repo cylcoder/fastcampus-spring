cb87b06728b46455c124a540b2272abf10cd9965
cylcoder <cylcoder@gmail.com>
2025-11-27 23:52:50 +0900

    Reset repository

--- memorydb/src/main/java/com/example/memorydb/book/db/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.memorydb.book.db.repository;
-
-import com.example.memorydb.book.db.entity.BookEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends JpaRepository<BookEntity, Long> {
-}
