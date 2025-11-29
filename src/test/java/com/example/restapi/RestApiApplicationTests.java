dfa9a47f6411022b3670da48c142bbbf8db40057
cylcoder <cylcoder@gmail.com>
2025-11-29 22:21:56 +0900

    Chapter 3: ObjectMapper

--- src/test/java/com/example/restapi/RestApiApplicationTests.java
+++ src/test/java/com/example/restapi/RestApiApplicationTests.java
@@ -1,13 +1,25 @@
 package com.example.restapi;
 
+import com.example.restapi.model.UserRequest;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import tools.jackson.databind.ObjectMapper;
 
 @SpringBootTest
 class RestApiApplicationTests {
 
+  @Autowired
+  private ObjectMapper objectMapper;
+
   @Test
   void contextLoads() {
+    UserRequest userRequest = new UserRequest("john", 100, "john@example.com", true);
+    String json = objectMapper.writeValueAsString(userRequest);
+    System.out.println("json = " + json);
+
+    UserRequest dto = objectMapper.readValue(json, UserRequest.class);
+    System.out.println("dto = " + dto);
   }
 
 }
