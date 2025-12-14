b840d591f324ab804b6ee711c9682508a7c083cb
cylcoder <cylcoder@gmail.com>
2025-12-14 20:47:41 +0900

    Delete delivery

--- service/api/src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-import com.delivery.db.UserDto;
-
-public class Main {
-
-  public static void main(String[] args) {
-    UserDto userDto = new UserDto();
-  }
-
-}
--- service/db/src/main/java/com/delivery/db/UserDto.java
+++
@@ -1,5 +0,0 @@
-package com.delivery.db;
-
-public record UserDto() {
-
-}
