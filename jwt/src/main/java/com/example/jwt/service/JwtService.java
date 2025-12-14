eac0b7ecda3418bf1f00f0278bd68221d286542c
cylcoder <cylcoder@gmail.com>
2025-12-14 19:10:21 +0900

    Chapter 1: JWT Token 인증

--- jwt/src/main/java/com/example/jwt/service/JwtService.java
+++ jwt/src/main/java/com/example/jwt/service/JwtService.java
@@ -1,6 +1,62 @@
 package com.example.jwt.service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.Map;
+import javax.crypto.SecretKey;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+@Service
+@Slf4j
 public class JwtService {
+
+  private static final String SECRET_KEY = "TG9yZW0gSXBzdW0gaXMgc2ltcGx5IGR1bW15IHRleHQgb2YgdGhlIHByaW50aW5nIGFuZCB0eXBlc2V0dGluZyBpbmR1c3RyeS4";
+  private static final SecretKey KEY = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+
+  public String generateToken(
+      Map<String, Object> claims,
+      LocalDateTime localExpiredAt
+  ) {
+    Date dateExpiredAt = Date.from(localExpiredAt.atZone(ZoneId.systemDefault()).toInstant());
+
+    return Jwts.builder()
+        .signWith(KEY, SignatureAlgorithm.HS256) // SecretKey와 Algorithm을 통해 서명
+        .setClaims(claims) // 사용자 정보를 Payload에 설정
+        .setExpiration(dateExpiredAt)
+        .compact();
+  }
+
+  public void validate(String jwt) {
+    JwtParser jwtParser = Jwts.parserBuilder() // 서명 검증을 하는 Parser 생성
+        .setSigningKey(KEY) // Secret Key 설정
+        .build();
+
+    /*
+    * 1. 서명 검증
+    * 2. 만료 시간 검증
+    * */
+    try {
+      Jws<Claims> claimsJws = jwtParser.parseClaimsJws(jwt);
+      claimsJws.getBody()
+          .entrySet()
+          .forEach(entry -> log.info("entry={}", entry));
+    } catch (SignatureException e) {
+      throw new IllegalArgumentException("Invalid JWT");
+    } catch (ExpiredJwtException e) {
+      throw new IllegalArgumentException("Expired JWT");
+    } catch (Exception e) {
+      throw new IllegalArgumentException("JWT parsing failed");
+    }
+  }
 
 }
--- jwt/src/test/java/com/example/jwt/JwtApplicationTests.java
+++ jwt/src/test/java/com/example/jwt/JwtApplicationTests.java
@@ -1,13 +1,34 @@
 package com.example.jwt;
 
+import com.example.jwt.service.JwtService;
+import java.time.LocalDateTime;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JwtApplicationTests {
 
+  @Autowired
+  JwtService jwtService;
+
   @Test
   void contextLoads() {
+  }
+
+  @Test
+  void jwt() {
+    Map<String, Object> claims = Map.of(
+        "username", "john",
+        "gender", "male"
+    );
+    LocalDateTime localExpiredAt = LocalDateTime.now().plusMinutes(1);
+
+    String jwt = jwtService.generateToken(claims, localExpiredAt);
+    System.out.println("jwt = " + jwt);
+
+    jwtService.validate(jwt);
   }
 
 }
